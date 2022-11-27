af1a9f0dad6f3d760e9da94c355b51bfdd0e5c13
Nurhadi27 <nrhdqmrd@gmail.com>
2022-11-28 06:12:07 +0700

    awal

---
+++ resources/views/berita.blade.php
@@ -0,0 +1,52 @@
+@extends('layout.app')
+@section('main')
+<section class="beurita">
+    <div class="gamb"><img style="width: 100%; height: 200px;" src="https://i2.wp.com/blog.tripcetera.com/id/wp-content/uploads/2020/10/pulau-padar.jpg"></div>
+<div class="cards">
+         <div class="services">
+            <div class="contenti text-center">
+              <img src="{{ asset('/img/beranda/HEHA.png') }}" class="card-img-top" alt="...">
+              <div class="contenti-body">
+                <h5 class="contenti-title">HeHa Ocean View</h5>
+                <p class="contenti-text">Ini isinya berita apa aja maunya maneh</p>
+                <hr class="hre">
+                <p class="contenti-textw"><img src="{{ asset('/img/beranda/jam.png') }}" alt=""> 17 Agustus 1945</p>
+              </div>
+            </div>
+            <div class="contenti text-center">
+              <img src="{{ asset('/img/beranda/HEHA.png') }}" class="card-img-top" alt="...">
+              <div class="contenti-body">
+                <h5 class="contenti-title">HeHa Ocean View</h5>
+                <p class="contenti-text">Ini isinya berita apa aja maunya maneh</p>
+                <hr class="hre">
+                <p class="contenti-textw"><img src="{{ asset('/img/beranda/jam.png') }}" alt=""> 17 Agustus 1945</p>
+              </div>
+            </div>
+            <div class="contenti text-center">
+              <img src="{{ asset('/img/beranda/HEHA.png') }}" class="card-img-top" alt="...">
+              <div class="contenti-body">
+                <h5 class="contenti-title">HeHa Ocean View</h5>
+                <p class="contenti-text">Ini isinya berita apa aja maunya maneh</p>
+                <hr class="hre">
+                <p class="contenti-textw"><img src="{{ asset('/img/beranda/jam.png') }}" alt=""> 17 Agustus 1945</p>
+              </div>
+            </div>
+            <div class="contenti text-center">
+              <img src="{{ asset('/img/beranda/HEHA.png') }}" class="card-img-top" alt="...">
+              <div class="contenti-body">
+                <h5 class="contenti-title">HeHa Ocean View</h5>
+                <p class="contenti-text">Ini isinya berita apa aja maunya maneh</p>
+                <hr class="hre">
+                <p class="contenti-textw"><img src="{{ asset('/img/beranda/jam.png') }}" alt=""> 17 Agustus 1945</p>
+              </div>
+            </div>
+         </div>
+      </div>
+</section>
+@endsection
+@section('js')
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+    <script src="js/splide.min.js"></script>
+    <script src="js/swiper-bundle.min.js"></script>
+    <script src="js/scripte.js"></script>
+@endsection
